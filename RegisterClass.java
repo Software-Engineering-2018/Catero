373651c05d03b3942b20ac96f659698c094e537b
Ashish Bisht <36641384+Ashish0809@users.noreply.github.com>
2018-11-23 20:30:19 +0530

    Register class test v1

---
+++ RegisterClass.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterClass
+ */
+@WebServlet("/reg")
+public class RegisterClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		String number=request.getParameter("contact");
+		String email_id=request.getParameter("email");
+		
+		PrintWriter out=response.getWriter();
+		out.write("<html>");
+		out.write("<body>");
+		out.write("Username: "+name);
+		out.write("Password: "+pass);
+		out.write("Contact: "+number);
+		out.write("Email ID: "+email_id);
+		
+	}
+
+}
